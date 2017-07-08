342605623f9edbf8e0ce5be878193d7bbd53a2e9
YannickLeRoux <rasyann@hotmail.com>
2017-07-07 23:51:28 -0400

    Remove special char

--- googleSearch.py
+++ googleSearch.py
@@ -8,22 +8,16 @@ Created on Fri Jul  7 19:38:06 2017
 
 import webbrowser
 
-term_list = "01. Coyote - Eve (The Kenneth Bager Experience Remix), \
-02. Death On The Balcony - Addict For Your Love, \
-03. Caribou - Your Love Will Set You Free (C2's Set U Free Remix), \
-04. Rene Bourgeois - Soul Paradise (Sascha Braemer's Deep Sunrise Remix), \
-05. Purple Disco Machine - Musique,\
-06. Pete Tong feat. S.Y.F. - Dawn (Franky Rizardo Remix),\
-07. DJ-Chart - Deep House Love,\
-08. Luca Guerrieri - Harmony,\
-09. Harry Romero & Joeski feat. Shawnee Taylor - Get It Right (Club Mix),\
-10. Mike Mago - The Gift (Mark Knight Remix),\
-11. Weiss - Our Love,\
-12. Joey Negro feat. Alex Mills - Everybody Wants Something (Akabu Warehouse Mix),\
-13. Franky Rizardo - Real Love,\
-14. Audion feat. Troels Abrahamsen - Dem Howl (Joris Voorn Mix),"
+#J ai besoin d ajouter \ manuellemt so far, trouver une solution
+#reprendre au podcast 30
 
-trackList = term_list.split(',')
+term_list = ""
+
+
+cleaned_term_list = term_list.translate ({ord(c): "+" for c in "!@#ü$%^&*()[]{};:,/<>?\|`~-=_+1234567890é"})
+
+
+trackList = cleaned_term_list.split('.')
 newList =[]
 
 for i in trackList:
